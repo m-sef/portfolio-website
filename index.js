cfc4a7fc82a721355434d37f08469cc274d232ed
m-sef <seth.l.moore0506@gmail.com>
2025-11-25 21:42:05 -0500

    Changes I forgot what I changed but definitely changes

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const panels = document.querySelectorAll('[role="tabpanel"]');
 selectTab(tabs[0])
 
 tabs.forEach((tab) => {
-    tab.addEventListener("click", function() {
-        selectTab(tab);
-    })
+  tab.addEventListener("click", function() {
+    selectTab(tab);
+  })
 })
 
 function selectTab(selectedTab) {
-    // Set "aria-selected" on all tabs to "false"
-    tabs.forEach((tab) => {
-        tab.setAttribute("aria-selected", "false");
-    })
+  // Set "aria-selected" on all tabs to "false"
+  tabs.forEach((tab) => {
+    tab.setAttribute("aria-selected", "false");
+  })
 
-    // Hide all panels
-    panels.forEach((panel) => {
-        panel.setAttribute("hidden", true);
-    })
+  // Hide all panels
+  panels.forEach((panel) => {
+    panel.setAttribute("hidden", true);
+  })
 
-    // Set "aria-selected" on selectedTab to "true"
-    selectedTab.setAttribute("aria-selected", "true");
+  // Set "aria-selected" on selectedTab to "true"
+  selectedTab.setAttribute("aria-selected", "true");
 
-    // Set the corresponding tab panel to not hidden
-    document.querySelector(`[role="tabpanel"][data-tab="${selectedTab.dataset.tab}"]`).removeAttribute("hidden")
+  // Set the corresponding tab panel to not hidden
+  document.querySelector(`[role="tabpanel"][data-tab="${selectedTab.dataset.tab}"]`).removeAttribute("hidden")
 }
